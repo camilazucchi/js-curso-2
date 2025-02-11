5fcf034bea6013dc9797ee9558fc154a217b12a2
camilazci <camilazucchi@outlook.com>
2025-02-11 14:41:37 -0300

    feat: add challenge 3 (onClick function) and extra button manipulation in HTML

--- app.js
+++ app.js
@@ -3,7 +3,20 @@
 const titulo = document.querySelector('h1');
 titulo.innerHTML = 'Hora do Desafio';
 
-// Desafio 2: Crie uma função que exiba no console a mensagem "O botão foi clicado" sempre que o botão Chutar for pressionado
+// Não estava no desafio, quis mudar para seguir corretamente o Desafio 2
+const botao1 = document.querySelector('button');
+botao1.innerHTML = 'Console';
+
+// Não estava no desafio, quis mudar para seguir corretamente o Desafio 3
+const botao2 = document.getElementById('reiniciar');
+botao2.innerHTML = 'Alerta';
+
+// Desafio 2: Crie uma função que exiba no console a mensagem "O botão foi clicado" sempre que o botão Console for pressionado
 function verificarChute() {
     console.log('O botão foi clicado');
+}
+
+// Desafio 3: Crie uma função que exiba um alerta com a mensagem "Eu amo JS", sempre que o botão Alerta for pressionado
+function exibirAlerta() {
+    alert('Eu amo JS');
 }
