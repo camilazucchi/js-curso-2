71a1c2c95b938eb94b42e5af9b63fbcba2792952
camilazci <camilazucchi@outlook.com>
2025-02-11 14:35:46 -0300

    feat: add challenge 1 (querySelector) and challenge 2 (onClick function)

---
+++ app.js
@@ -0,0 +1,9 @@
+// Desafios: hora da prática
+// Desafio 1: Altere o conteúdo da tag h1 com document.querySelector
+const titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+// Desafio 2: Crie uma função que exiba no console a mensagem "O botão foi clicado" sempre que o botão Chutar for pressionado
+function verificarChute() {
+    console.log('O botão foi clicado');
+}
